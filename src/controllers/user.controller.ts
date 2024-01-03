c428f6c4b02f2b3c366fbfddc081cdb8ab8caad3
abdullahma1 <abdullahjawed900@gmail.com>
2024-01-03 12:00:00 +0500

    User Created

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,19 @@
 import { Request , Response  } from "express";
+import User from "../models/User";
 
 
 const createUser = async (req:Request , res :Response)=>{
-// check if user already exisit
-//if not created it
-//return the user object on calling the client
+
+    const {email , anem ,addressLine1,city,country} = req.body;
+
+    if(email){
+        
+        throw new Error("User already exist")
+    }
+    const userCreated = await User.create(req.body)
+    // check if user already exisit
+    //if not created it
+    //return the user object on calling the client
 
     console.log("user created")
 };
