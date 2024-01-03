dd5fdf5e835cdef16a8148faa513071f5b46fb7e
abdullahma1 <abdullahjawed900@gmail.com>
2024-01-03 12:00:00 +0500

    Create User Models

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { Request , Response  } from "express";
+
+
+const createUser = async (req:Request , res :Response)=>{
+// check if user already exisit
+//if not created it
+//return the user object on calling the client
+
+    console.log("user created")
+};
+
+export default createUser
+
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import express , {Request , Response} from 'express';
-import cors  from 'cors';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
-
-
-mongoose.connect(process.env.CONNECTION_STRING as string)
+import userRoute from './routes/user.route'
+mongoose
+ .connect(process.env.CONNECTION_STRING as string)
+.then(() => { console.log("connected to the database") })
 
 const app = express();
 app.use(express.json())
 app.use(cors());
+const PORT = process.env.PORT || 3000
 
-app.get("/test", async (req:Request,res:Response)=>{
-    res.json({message:"Hello!"})
-});
 
-app.listen(4000,()=>{
-    console.log("server is running")
+
+app.use("/api/user",userRoute)
+// app.get("/test", async (req: Request, res: Response) => {
+//     res.json({ message: "Hello!" })
+// });
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
 })
